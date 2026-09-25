bd92a4ae8b8274041b57555da6f7760c8eafdcfd
Bogdan Perederii <boperederii@gmail.com>
2026-09-25 13:11:29 +0200

    Fix CSS loading on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bpunleashed.github.io',
+  base: '/portfolio',
+  build: {
+    assets: 'assets' // Ukladá CSS do /assets namiesto /_astro
+  },
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [mdx()]
 });
